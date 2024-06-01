8b969538d13f2fcda31881b8bc97bbcac9b11bab
Farooqking302 <sindhifarooq67@gmail.com>
2024-06-01 19:16:03 +0500

    Update Tempmails.js

--- commands/Tempmails.js
+++ commands/Tempmails.js
@@ -23,7 +23,7 @@ france({ nomCom: "tempmail", reaction: "😌", nomFichier: __filename }, async (
     let z = '*FLASH-MD* Temporary emails,Powered by *France King* \n\n ' + "Click the links below to create unlimited TEMPORARY Emails. Powered by *FLASH-MD.*\n\n";
     let d = ' 1️⃣ https://tempumail.com\n\n 2️⃣ https://etempmail.com\n\n 3️⃣ https://ghostmail.one\n\n 4️⃣ https://tempmailid.com';
     let varmess = z + d;
-    var img = 'https://telegra.ph/file/f1f2303ff4e39b0a3b6b3.jpg';
+    var img = 'https://telegra.ph/file/148dad84e133b512b4b3b.jpg';
     await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
     //console.log("montest")
 });
